2d1fc8700a5504da716e9007455e9ee13a8ba70b
Shruti Shetty <shruti.shetty@attivio.com>
2019-02-08 14:42:15 -0500

    Adding let, const and const arrays

---
+++ app/index.js
@@ -0,0 +1,15 @@
+ /** let which helps u store variables which you can change */
+ 
+ let limit = 100;
+ limit += 200;
+ console.log(limit);
+
+/**const values cannot be changes */
+ const limit2 = 100;
+ console.log(200);
+ 
+ /**you cannot vhange values for const but can add values to it if its an array */
+ const emails= ['shr@emails.com','she@emails.com'];
+ console.log(emails);
+ emails.push('uti@gmail.com');
+ console.log(emails);
